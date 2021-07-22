2d53cbc54945854cf2e4575e81512cc93000042c
김주호 <@DESKTOP-6I4CEJ9>
2021-07-22 12:16:50 +0900

    20210722_새로운거

--- src/com/greedy/git/Application.java
+++ src/com/greedy/git/Application.java
@@ -7,12 +7,7 @@ public class Application {
 		System.out.println("깃허브 올라가나");
 		System.out.println("한줄 추가하기");
 		System.out.println("또 한줄 추가");
-<<<<<<< HEAD
 		System.out.println("나도 코딩한다");
-		System.out.println("어떤 상황인지 모르고 코딩중");
-=======
-		System.out.println("계속 코딩중");
->>>>>>> refs/remotes/origin/master
 	}
 
 }
