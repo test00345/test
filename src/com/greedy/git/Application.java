6fe66e39cebecf7d0b3ea2eb01c8343a8ace8710
김주호 <@DESKTOP-6I4CEJ9>
2021-07-22 11:29:16 +0900

    2021072211_세번째 커밋 & 푸쉬

--- src/com/greedy/git/Application.java
+++ src/com/greedy/git/Application.java
@@ -3,9 +3,9 @@ package com.greedy.git;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
 		
 		System.out.println("깃허브 올라가나");
+		System.out.println("한줄 추가하기");
 	}
 
 }
